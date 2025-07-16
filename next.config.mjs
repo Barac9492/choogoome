7f76cf48826ac2b0c1cfb7cf8fc8fdce9378480d
Yeo Joon Cho <yeojooncho@Yeoui-MacBookAir.local>
2025-07-16 14:17:22 +0900

    chore(config): enable serverActions via next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // enable Next.js Server Actions
+    serverActions: {},
+  },
+};
+
+export default nextConfig;
